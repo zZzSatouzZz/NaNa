50ef04b027d69c3102a07006094d94aab4873520
zZzSatouzZz <lolvlogs947@gmail.com>
2024-09-20 22:41:28 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,265 @@
+let noButtonClickCount = 0; 
+let noButtonState = 0; 
+
+
+document.getElementById('gifContainer').style.display = 'block';
+
+document.getElementById('siBtn').addEventListener('click', function() {
+    
+    document.getElementById('sadGifContainer').style.display = 'none';
+    document.getElementById('sadGifContainer1').style.display = 'none';
+    document.getElementById('sadGifContainer2').style.display = 'none';
+    document.getElementById('gifContainer').style.display = 'none';
+    document.getElementById('happyGifContainer').style.display = 'block';
+
+   
+    document.getElementById('question').style.display = 'none';
+    document.getElementById('siBtn').style.display = 'none';
+    document.body.classList.add('bg-green');
+    document.getElementById('noBtn').style.display = 'none';
+
+    
+    document.getElementById('messageContainer').style.display = 'block';
+    document.getElementById('messageContainer').innerHTML = 'tui hông ngờ tới luôn đó, iu bé lắm luônnn:3';
+
+    
+    setTimeout(function() {
+        document.getElementById('happyGifContainer').style.display = 'none';
+        document.getElementById('happyGifContainer2').style.display = 'block';
+    }, 1000);
+
+    setTimeout(function() {
+        document.getElementById('happyGifContainer2').style.display = 'none';
+        document.getElementById('happyGifContainer3').style.display = 'block';
+    }, 2000);
+    setTimeout(function() {
+        document.getElementById('happyGifContainer3').style.display = 'none';
+        document.getElementById('happyGifContainer4').style.display = 'block';
+    }, 3000);
+  
+});
+
+
+document.getElementById('noBtn').addEventListener('click', function() {
+    switch (noButtonState) {
+        case 0:
+            
+            document.getElementById('happyGifContainer').style.display = 'none';
+            document.getElementById('gifContainer').style.display = 'none';
+            document.getElementById('sadGifContainer').style.display = 'block';
+
+            
+            document.getElementById('noBtn').innerHTML = 'Cho anh cơ hội đii mò';
+            document.getElementById('noBtn').style.backgroundColor = '#F1330A';
+          
+            
+            document.getElementById('siBtn').style.fontSize = '40px';
+            document.getElementById('siBtn').style.padding = '20px 40px';
+       
+            
+           
+
+        
+            noButtonClickCount++;
+            noButtonState++;
+            break;
+
+        case 1:
+            
+
+            document.getElementById('noBtn').innerHTML = 'tui hứa sẽ iuu bé suốt đời đó';
+            document.getElementById('noBtn').style.backgroundColor = '#F1330A';
+            document.getElementById('sadGifContainer').style.display = 'none';
+            document.getElementById('sadGifContainer2').style.display = 'block';
+
+            
+            document.getElementById('siBtn').style.fontSize = '50px';
+            document.getElementById('siBtn').style.padding = '30px 50px';
+        
+            noButtonState++;
+            break;
+
+        case 2:
+           
+            document.getElementById('noBtn').innerHTML = 'iuu Bé hết mực nữaaaa';
+            document.getElementById('noBtn').style.backgroundColor = '#F1330A';
+            document.getElementById('sadGifContainer').style.display = 'none';
+            document.getElementById('sadGifContainer2').style.display = 'none';
+            document.getElementById('sadGifContainer1').style.display = 'block';
+
+            
+            document.getElementById('siBtn').style.fontSize = '60px';
+            document.getElementById('siBtn').style.padding = '40px 60px';
+        
+            noButtonState++;
+            break;
+        
+        case 3:
+           
+            document.getElementById('noBtn').innerHTML = 'Vậy nên choo tui 1 cơ hội nhé!';
+            document.getElementById('noBtn').style.backgroundColor = '#F1330A';
+
+           
+            document.getElementById('siBtn').style.fontSize = '70px';
+            document.getElementById('siBtn').style.padding = '50px 70px';
+        
+            noButtonState++;
+            break;
+        case 4:
+           
+            document.getElementById('noBtn').innerHTML = 'Thật sự bé không đồng ý saooo?:((';
+            document.getElementById('noBtn').style.backgroundColor = '#F1330A';
+
+           
+            document.getElementById('siBtn').style.fontSize = '80px';
+            document.getElementById('siBtn').style.padding = '60px 80px';
+         
+            noButtonState++;
+            break;
+        case 5:
+           
+            document.getElementById('noBtn').innerHTML = 'bé muốn bỏ rơi chàng trai này ư hỏ :((';
+            document.getElementById('noBtn').style.backgroundColor = '#F1330A';
+
+           
+            document.getElementById('siBtn').style.fontSize = '90px';
+            document.getElementById('siBtn').style.padding = '70px 90px';
+
+            noButtonState++;
+            break;
+        case 6:
+           
+            document.getElementById('noBtn').innerHTML = 'Đừng anh xin bé đóooooo hiuhiu';
+            document.getElementById('noBtn').style.backgroundColor = '#F1330A';
+
+            
+            document.getElementById('siBtn').style.fontSize = '100px';
+            document.getElementById('siBtn').style.padding = '80px 100px';
+            
+            noButtonState++;
+            break;
+        case 7:
+           
+            document.getElementById('noBtn').innerHTML = 'Thật sự tuii rất iu bé mà';
+            document.getElementById('noBtn').style.backgroundColor = '#F1330A';
+
+            
+            document.getElementById('siBtn').style.fontSize = '120px';
+            document.getElementById('siBtn').style.padding = '90px 120px';
+  
+            noButtonState++;
+            break;
+        case 8:
+           
+            document.getElementById('noBtn').innerHTML = 'Anh sẽ bùnnn lém nha đóooooo';
+            document.getElementById('noBtn').style.backgroundColor = '#F1330A';
+
+            document.getElementById('siBtn').style.fontSize = '140px';
+            document.getElementById('siBtn').style.padding = '100px 140px';
+       
+            noButtonState++;
+            break;
+
+        case 9:
+           
+            document.getElementById('noBtn').innerHTML = 'Bé ơiii , cho Anh cơ hội diiii';
+            document.getElementById('noBtn').style.backgroundColor = '#F1330A';
+
+            
+            document.getElementById('siBtn').style.fontSize = '160px';
+            document.getElementById('siBtn').style.padding = '110px 160px';
+          
+            noButtonState++;
+            break;
+
+        case 10:
+           
+            document.getElementById('noBtn').innerHTML = 'hãy chấp nhận tình iuu của Anh nhóo';
+            document.getElementById('noBtn').style.backgroundColor = '#F1330A';
+
+         
+            document.getElementById('siBtn').style.fontSize = '180px';
+            document.getElementById('siBtn').style.padding = '120px 180px';
+    
+            noButtonState++;
+            break;
+        case 11:
+           
+            document.getElementById('noBtn').innerHTML = 'Anh thật sự rất và rất iuu em đó';
+            document.getElementById('noBtn').style.backgroundColor = '#F1330A';
+
+            document.getElementById('siBtn').style.fontSize = '200px';
+            document.getElementById('siBtn').style.padding = '130px 200px';
+            
+            noButtonState++;
+            break;
+        
+        case 12:
+           
+            document.getElementById('noBtn').innerHTML = 'Đừng bỏ rơi chàng trai đáng thương này mà';
+            document.getElementById('noBtn').style.backgroundColor = '#F1330A';
+
+            
+            document.getElementById('siBtn').style.fontSize = '220px';
+            document.getElementById('siBtn').style.padding = '140px 220px';
+    
+            noButtonState++;
+            break;
+        
+        case 13:
+           
+            document.getElementById('noBtn').innerHTML = 'Em đồng ý nhóooooo';
+            document.getElementById('noBtn').style.backgroundColor = '#F1330A';
+
+            document.getElementById('siBtn').style.fontSize = '240px';
+            document.getElementById('siBtn').style.padding = '150px 240px';
+         
+            noButtonState++;
+            break;
+        
+        case 14:
+           
+            document.getElementById('noBtn').innerHTML = 'Chỉ một lần và lần này thoaii';
+            document.getElementById('noBtn').style.backgroundColor = '#F1330A';
+
+           
+            document.getElementById('siBtn').style.fontSize = '260px';
+            document.getElementById('siBtn').style.padding = '160px 260px';
+            
+            noButtonState++;
+            break;
+        
+        case 15:
+           
+            document.getElementById('noBtn').innerHTML = 'Nhaaaaaaaaaaaaaaaaaaaa';
+            document.getElementById('noBtn').style.backgroundColor = '#F1330A';
+
+            
+            document.getElementById('siBtn').style.fontSize = '280px';
+            document.getElementById('siBtn').style.padding = '170px 280px';
+          
+            noButtonState++;
+            break;
+        
+     
+        case 16:
+                document.getElementById('noBtn').innerHTML = 'Điiiiii mà emmmmmmmm';
+                document.getElementById('noBtn').style.backgroundColor = '#F1330A';
+              
+                document.getElementById('sadGifContainer').style.display = 'none';
+                document.getElementById('sadGifContainer1').style.display = 'none';
+                document.getElementById('sadGifContainer2').style.display = 'none';
+                document.getElementById('gifContainer').style.display = 'block';
+                document.getElementById('happyGifContainer').style.display = 'none';
+                
+                noButtonState = 0;
+                break;
+        
+
+        default:
+            
+            break;
+    }
+});
+
+
